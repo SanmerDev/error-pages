3a167ae468b6b0f10c25266d6bd4030b85a13dc1
SanmerDev <sanmer@sanmer.dev>
2024-05-19 16:05:01 +0800

    Impl `Logger`

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 use actix_web::http::StatusCode;
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpResponse, HttpServer, Responder};
 
 const TEMPLATE: &'static str = include_str!("../html/template.html");
 
@@ -15,8 +15,19 @@ async fn index(content: web::Path<String>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind(("0.0.0.0", 8080))?
-        .run()
-        .await
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .init();
+
+    tracing::info!("starting HTTP server at http://0.0.0.0:8080");
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(index)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
